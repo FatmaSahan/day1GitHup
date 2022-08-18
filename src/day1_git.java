af9fb5b5da4c4b3b381fe5b04370abdb63153d82
fatma <ffsbm3842@gmail.com>
2022-08-18 12:45:04 +0100

    2.C

--- src/day1_git.java
+++ src/day1_git.java
@@ -1,5 +1,6 @@
 public class day1_git {
     public static void main(String[] args) {
         System.out.println("hello git 1. deneme");
+        System.out.println("hello git 2. deneme");
     }
 }
